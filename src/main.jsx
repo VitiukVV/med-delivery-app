547c0de6786d1400b7f5993dabafa45d7dcd4409
VolodymyrVitiuk <v.v.endeavour@gmail.com>
2024-03-03 22:30:46 +0200

    add settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./components/App.jsx";
-import { createHashRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import TestComponentForRouting from "./components/TestComponentForRouting.jsx";
-
-const router = createHashRouter([
-    {
-        path: "/",
-        element: <App />,
-    },
-    {
-        path: "/test",
-        element: <TestComponentForRouting />,
-    },
-]);
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <BrowserRouter basename="/med-delivery-app/">
+                    <App />
+                </BrowserRouter>
+            </PersistGate>
+        </Provider>
     </React.StrictMode>
 );
