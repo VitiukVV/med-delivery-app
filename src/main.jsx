a6390cd28afa0bbd2aabd06fc85b2c7106489dd1
VolodymyrVitiuk <v.v.endeavour@gmail.com>
2024-03-19 16:14:11 +0200

    fix deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./components/App.jsx";
 import "./index.css";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
